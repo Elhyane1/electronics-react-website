207e60069a3a5b3e5dfa933d06588924e8929d2c
othman <businessothman1@gmail.com>
2025-03-10 01:20:42 +0100

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-const withMT = require("@material-tailwind/react/utils/withMT");
+// import withMT from "@material-tailwind/react/utils/withMT";
 
 
-module.exports = withMT({
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
+// module.exports = withMT({
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{vue,js,ts,jsx,tsx}",
+//     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+//     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// });
 
 // https://vite.dev/config/
 export default defineConfig({
